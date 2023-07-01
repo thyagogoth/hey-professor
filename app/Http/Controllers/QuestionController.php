a9b6817b564fa9a8b94100ec4e09be19864384e1
Thiago F. da Rosa <thyagogoth@gmail.com>
2023-07-01 17:18:13 -0300

    should check if ends with question mark ?

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,22 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Illuminate\Http\Request;
+use Closure;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-
     public function store(Request $request): RedirectResponse
     {
 
         Question::query()->create(
             $request->validate([
-                'question' => ['required', 'min:10'],
+                'question' => [
+                    'required',
+                    'min:10',
+                    function (string $attribute, mixed $value, Closure $fail) {
+                        if ($value[strlen($value) - 1] != '?') {
+                            $fail('Are you sure that is a question? It is missing the question mark.');
+                        }
+                    },
+                ],
             ])
         );
 
         return to_route('dashboard');
     }
-
 }
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -7,39 +7,49 @@ use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\post;
 
 // test || it
-it('should be able to create a new question bigger than 255 characters', function() {
+it('should be able to create a new question bigger than 255 characters', function () {
     // Arrange > preparar
     $user = User::factory()->create();
     actingAs($user);
 
     // Act :  Agir
     $request = post(route('question.store'), [
-        'question' => str_repeat('*', 260). '?',
+        'question' => str_repeat('*', 260).'?',
     ]);
 
     // Assert : verificar
     $request->assertRedirect(route('dashboard'));
     assertDatabaseCount('questions', 1);
-    assertDatabaseHas('questions', ['question' => str_repeat('*', 260). '?']);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260).'?']);
 });
 
-it ( 'should check if ends with question mark ?', function () {
-
-});
-
-it ( 'should have at least 10 characters', function () {
+it('should check if ends with question mark ?', function () {
     // Arrange > preparar
     $user = User::factory()->create();
     actingAs($user);
 
     // Act :  Agir
     $request = post(route('question.store'), [
-        'question' => str_repeat('*', 8). '?',
+        'question' => str_repeat('*', 10),
+    ]);
+
+    // Assert : verificar
+    $request->assertSessionHasErrors(['question' => 'Are you sure that is a question? It is missing the question mark.']);
+    assertDatabaseCount('questions', 0);
+});
+
+it('should have at least 10 characters', function () {
+    // Arrange > preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act :  Agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8).'?',
     ]);
 
     // Assert : verificar
     $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
     assertDatabaseCount('questions', 0);
-    // assertDatabaseHas('questions', ['question' => str_repeat('*', 260). '?']);
 
 });

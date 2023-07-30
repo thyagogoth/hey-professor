6872b9fccc8e7ab5fea3fef9a3af0ec1ec2dc0e8
Thiago Fernando da Rosa <thyagogoth@gmail.com>
2023-07-30 19:10:43 -0300

    it should order by like and unlike, most liked question should be at the top and most unliked question should be in the bottom

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
@@ -10,9 +11,17 @@ class DashboardController extends Controller
     public function __invoke(): View
     {
         return view('dashboard', [
-            'questions' => Question::withSum('votes', 'like')
+            'questions' => Question::query()
+                ->when(request()->has('search'), function (Builder $query) {
+                    $query->where('question', 'like', '%' . request()->search . '%');
+                })
+                ->withSum('votes', 'like')
                 ->withSum('votes', 'unlike')
-                ->paginate(5),
+                ->orderByRaw('
+                    case when votes_sum_like is null then 0 else votes_sum_like end desc,
+                    case when votes_sum_unlike is null then 0 else votes_sum_unlike end
+                ')
+                ->paginate(25),
         ]);
     }
 
--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -31,3 +31,27 @@ it('should paginate the result', function () {
     get(route('dashboard'))
         ->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator);
 });
+
+it('should order by like and unlike, most liked question should be at the top and most unliked question should be in the bottom', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+
+    $mostLikedQuestion = Question::find(3);
+    $user->like($mostLikedQuestion);
+
+    $mostUnlikedQuestion = Question::find(1);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    actingAs($user);
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
+        });
+});
